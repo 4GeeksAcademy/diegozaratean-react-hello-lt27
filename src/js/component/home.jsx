336d0912935c8b8cc3a51f3f033337eb7366009d
diego <diegozaratean@gmail.com>
2024-03-07 02:06:24 +0000

    finish react class 1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+// 4. Importar comoponente 
+import Nav from './nav.jsx'
+
+
+// 5. Utilizar el componente
 
 //create your first component
 const Home = () => {
 	return (
-			<h1 className="text-center mt-5">Hola Bienvenidos a React Js ;)</h1>
+		<>
+			<Nav />
+			<h1 className="text-center mt-5">Hola Bienvenidos a React Js ;) 123</h1>
+		</>
 	);
 };
 
