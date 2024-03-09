b5d29c823650f8e89fc6d66fde5b47c9c2dff4e2
diego <diegozaratean@gmail.com>
2024-03-09 02:17:13 +0000

    finish react component class

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 // 4. Importar comoponente 
 import Nav from './nav.jsx'
-
+import Jumbotron from './jumbotron.jsx'
+import Button from './button.jsx'
 
 // 5. Utilizar el componente
 
@@ -10,7 +11,9 @@ const Home = () => {
 	return (
 		<>
 			<Nav />
+			<Jumbotron />
 			<h1 className="text-center mt-5">Hola Bienvenidos a React Js ;) 123</h1>
+			<Button />
 		</>
 	);
 };
